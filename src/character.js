946816ba8657794b687f2d156049ef4ed1e3c68d
Farshad <f.bagdeli13@gmail.com>
2024-06-11 14:32:17 +0200

    Character class Done

---
+++ src/character.js
@@ -0,0 +1,23 @@
+class Character {
+  #id
+  #name
+  #maxHitPoints
+  #curerentHitPoints
+  #damage
+  #takeDamage
+  constructor(name, maxHitPoints, damage) {
+    this.#name = name
+    this.#maxHitPoints = maxHitPoints
+    this.#curerentHitPoints = maxHitPoints
+    this.#damage = damage
+  }
+
+  takeDamage(attacker) {
+    this.#curerentHitPoints -= attacker.damage
+    if(this.#curerentHitPoints < 0) {
+      this.#curerentHitPoints = 0
+    }
+  }
+}
+
+export { Character }
